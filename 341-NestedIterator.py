15802ed9757c40973158c31236df78d5404d6008
zoushuang86 <zoushuang86@163.com>
2021-10-22 16:44:10 +0800

    20211022

--- 341-NestedIterator.py
+++ 341-NestedIterator.py
@@ -41,30 +41,39 @@ return res
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-# class NestedInteger:
-#    def isInteger(self) -> bool:
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        """
-#
-#    def getInteger(self) -> int:
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        """
-#
-#    def getList(self) -> [NestedInteger]:
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        """
+class NestedInteger:
+   def isInteger(self) -> bool:
+       """
+       @return True if this NestedInteger holds a single integer, rather than a nested list.
+       """
+       pass
+
+   def getInteger(self) -> int:
+       """
+       @return the single integer that this NestedInteger holds, if it holds a single integer
+       Return None if this NestedInteger holds a nested list
+       """
+       pass
+
+   def getList(self) -> [NestedInteger]:
+       """
+       @return the nested list that this NestedInteger holds, if it holds a nested list
+       Return None if this NestedInteger holds a single integer
+       """
+       pass
+
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
+        self.stuck = nestedList[::-1]
 
     def next(self) -> int:
+        return self.stuck.pop(-1).getInteger()
 
     def hasNext(self) -> bool:
+        while len(self.stuck) > 0 and self.stuck[-1].isInteger() == False:
+            self.stuck += self.stuck.pop().getList()[::-1]
+        return len(self.stuck) > 0
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
