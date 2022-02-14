255baa8ae83bbd0554fc3077b89ba933f86fa270
zoushuang86 <zoushuang86@163.com>
2022-02-14 12:33:07 +0800

    20220214

---
+++ 540-singleNonDuplicate.py
@@ -0,0 +1,60 @@
+"""
+540. 有序数组中的单一元素
+给你一个仅由整数组成的有序数组，其中每个元素都会出现两次，唯有一个数只会出现一次。
+
+请你找出并返回只出现一次的那个数。
+
+你设计的解决方案必须满足 O(log n) 时间复杂度和 O(1) 空间复杂度。
+
+
+
+示例 1:
+
+输入: nums = [1,1,2,3,3,4,4,8,8]
+输出: 2
+示例 2:
+
+输入: nums =  [3,3,7,7,10,11,11]
+输出: 10
+
+
+提示:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+"""
+"""
+由于数组是有序的，因此数组中相同的元素一定相邻。对于下标 x 左边的下标 y，如果 nums[y]=nums[y+1]，则 y 一定是偶数；
+对于下标 x 右边的下标 z，如果 nums[z]=nums[z+1]，则 z 一定是奇数。由于下标 x 是相同元素的开始下标的奇偶性的分界，因此可以使用二分查找的方法寻找下标 x。
+
+初始时，二分查找的左边界是 0，右边界是数组的最大下标。每次取左右边界的平均值 mid 作为待判断的下标，根据 mid 的奇偶性决定和左边或右边的相邻元素比较：
+
+如果 mid 是偶数，则比较 nums[mid] 和 nums[mid+1] 是否相等；
+如果 mid 是奇数，则比较 nums[mid−1] 和 nums[mid] 是否相等。
+
+如果上述比较相邻元素的结果是相等，则 mid<x，调整左边界，否则 mid≥x，调整右边界。调整边界之后继续二分查找，直到确定下标 xx 的值。
+
+得到下标 x 的值之后，nums[x] 即为只出现一次的元素。
+
+细节
+利用按位异或的性质，可以得到 mid 和相邻的数之间的如下关系，其中 ⊕ 是按位异或运算符：
+当 mid 是偶数时，mid+1=mid⊕1；
+当 mid 是奇数时，mid−1=mid⊕1。
+因此在二分查找的过程中，不需要判断 mid 的奇偶性，mid 和 mid⊕1 即为每次需要比较元素的两个下标。
+"""
+class Solution:
+    def singleNonDuplicate(self, nums: list) -> int:
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right-left) // 2
+            if nums[mid] == nums[mid ^ 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
+
+
+if __name__ == "__main__":
+    nums = [1,1,2,3,3,4,4,8,8]
+    s = Solution()
+    print(s.singleNonDuplicate(nums))
